7da448d132503f0f4b377e3bf644ea368700e5d1
unknown <kimdongju3331@gmail.com>
2021-06-03 17:22:50 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App=()=>{
+
+
+  return (<>
+  {/* <nav>
+    <Link to="/">Home</Link>
+    <Link to="/">Blog</Link>
+    <Link to="/">About</Link>
+  </nav> */}
+
+  <h1>첫번째 크기 헤드라인</h1>
+  <h2>두번째 크기 헤드라인</h2>
+  <h3>세번째 크기 헤드라인</h3>
+  <h4>네번째 크기 헤드라인</h4>
+  <h5>다섯번째 크기 헤드라인</h5>
+    <p>문단은 p로쓰세요</p>
+  </>)
+}
+
+export default App
